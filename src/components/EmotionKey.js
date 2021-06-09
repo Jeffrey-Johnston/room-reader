b2574c04105f84b07ebbf6caa90880953f0cf438
Jeffrey Johnston <JJohnston1822@gmail.com>
2021-06-09 01:05:15 -0400

    mad-shit

--- src/components/EmotionKey.js
+++ src/components/EmotionKey.js
@@ -3,12 +3,12 @@ import classes from "./EmotionKey.module.css";
 const EmotionKey = () => {
   return (
     <section className={classes.key}>
-      <p>0%</p>
+      <p className={classes.zero}>0%</p>
       <div className={`${classes.bar} ${classes.red}`}></div>
       <div className={`${classes.bar} ${classes.orange}`}></div>
       <div className={`${classes.bar} ${classes.yellow}`}></div>
       <div className={`${classes.bar} ${classes.green}`}></div>
-      <p>100%</p>
+      <p className={classes.oneH}>100%</p>
     </section>
   );
 };
